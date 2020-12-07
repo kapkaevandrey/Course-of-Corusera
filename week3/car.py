1d6abadc361a379a352aae3bd64946fae6e95897
LuckyKAA <kapkaew@yandex.ru>
2020-12-07 11:32:08 +0300

    Add new function validation file name.

--- week3/car.py
+++ week3/car.py
@@ -47,9 +47,45 @@ class SpecMachine(BaseCar):
         super(SpecMachine, self).__init__("spec_machine",  brand, photo_file_name, carrying)
 
 
+def valid_photo(file_name):
+    try:
+        root, ext = os.path.splitext(file_name)
+        if ext in BaseCar._file_type:
+            return True
+        else:
+            return False
+    except IOError:
+        return False
 
 
-def get_car_list(file_name_csv):
+
+def get_car(data, data_row):
+    car = [data_row[i] for i in [1, 3, 5, 2]]
+    if not any(car):
+        return data
+    if valid_photo(car[1]):
+        data.append(Car(*car))
+    return data
+
+
+def get_truck(data, data_row):
+    truck = [data_row[i] for i in [1, 3, 5, 4]]
+    if not any(truck[:-1]):
+        return data
+    if valid_photo(truck[1]):
+        data.append(Truck(*truck))
+    return data
+
+
+def get_spec_car(data, data_row):
+    spec_machine = [data_row[i] for i in [1, 3, 5, 6]]
+    if not any(spec_machine):
+        return data
+    if valid_photo(spec_machine[1]):
+        data.append(SpecMachine(*spec_machine))
+    return data
+
+def get_car_list(file_name_csv:object):
     """
     Фнукция считывает файл в формате csv и собирает список из объектов
     :param file_name_csv:
@@ -57,17 +93,27 @@ def get_car_list(file_name_csv):
     """
     file_csv = open(file_name_csv, encoding="UTF-8")
     reader = csv.reader(file_csv, delimiter=";")
+    car_data = [row for row in reader]
+    file_csv.close()
+    data_len = len(car_data[0])
     car_list = []
-    for row in reader:
+    for row in car_data:
+        if len(row) != data_len:
+            continue
         if row[0] == "car":
-            car_list = get_car(car_list)
-        if row[0] == "truck":
-            car_list = get_truck(car_list)
-        if row[0] == "spec_machine":
-            car_list = get_spec_machine(car_list)
+            car_list = get_car(car_list, row)
+        elif row[0] == "truck":
+            car_list = get_truck(car_list, row)
+        elif row[0] == "spec_machine":
+            car_list = get_spec_car(car_list, row)
         else:
             continue
-        return car_list
+    return car_list
 
 
+if __name__ == "__main__":
+    cars = get_car_list("coursera_week3_cars.csv")
+    for car in cars:
+        print(type(car))
+        print(car.brand)
 
