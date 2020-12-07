8bf640dc41bc58f139c00ac8c9f064b0e2896210
LuckyKAA <kapkaew@yandex.ru>
2020-12-07 11:38:27 +0300

    Add new function validation file name.

--- week3/car.py
+++ week3/car.py
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
 import os
 import csv
+
 
 class BaseCar:
     _file_type = [".jpg", ".jpeg", ".gif", ".png"]
 
-    def __init__(self, car_type="unknown",brand="unknown",
+    def __init__(self, car_type="unknown", brand="unknown",
                  photo_file_name="unknown", carrying="unknown",):
         self.brand = brand
         self.photo_file_name = photo_file_name
@@ -16,11 +16,13 @@ class BaseCar:
         root, ext = os.path.splitext(self.photo_file_name)
         return ext
 
+
 class Car(BaseCar):
-    def __init__(self,brand="unknown",
+    def __init__(self, brand="unknown",
                  photo_file_name="unknown", carrying="unknown", passenger_seats_count=4):
         super(Car, self).__init__("car", brand, photo_file_name, carrying)
         self.passenger_seats_count = passenger_seats_count
+
 
 class Truck(BaseCar):
     def __init__(self, brand="unknown",
@@ -37,8 +39,8 @@ class Truck(BaseCar):
             self.body_height = 0.0
 
     def get_body_volume(self):
-        self.volume = self.body_length * self.body_width * self.body_height
-        return self.volume
+        volume = self.body_length * self.body_width * self.body_height
+        return volume
 
 
 class SpecMachine(BaseCar):
@@ -59,13 +61,12 @@ def valid_photo(file_name):
         return False
 
 
-
 def get_car(data, data_row):
-    car = [data_row[i] for i in [1, 3, 5, 2]]
-    if not any(car):
+    new_car = [data_row[i] for i in [1, 3, 5, 2]]
+    if not any(new_car):
         return data
-    if valid_photo(car[1]):
-        data.append(Car(*car))
+    if valid_photo(new_car[1]):
+        data.append(Car(*new_car))
     return data
 
 
@@ -86,7 +87,8 @@ def get_spec_car(data, data_row):
         data.append(SpecMachine(*spec_machine))
     return data
 
-def get_car_list(file_name_csv:object):
+
+def get_car_list(file_name_csv: object):
     """
     Фнукция считывает файл в формате csv и собирает список из объектов
     :param file_name_csv:
@@ -117,58 +119,3 @@ if __name__ == "__main__":
     for car in cars:
         print(type(car))
         print(car.brand)
-
-=======
-import os
-
-class BaseCar:
-
-    _file_type = [".jpg", ".jpeg", ".gif", ".png"]
-
-    def __init__(self, car_type="unknown",brand="unknown",
-                 photo_file_name="unknown", carrying="unknown",):
-        self.brand = brand
-        self.photo_file_name = photo_file_name
-        self.carrying = carrying
-        self.car_type = car_type
-
-    def get_photo_file_ext(self):
-        try:
-            root, ext = os.path.splitext(self.photo_file_name)
-        except IOError:
-            return "File not exist"
-        if ext not in self._file_type:
-            ext = "unknown"
-            return f"invalid format. Format of file must be {','.join(self._file_type)} not {ext}"
-        else:
-            return ext
-
-class Car(BaseCar):
-    def __init__(self,brand="unknown",
-                 photo_file_name="unknown", carrying="unknown", passenger_seats_count=4):
-        super(Car, self).__init__("Car", brand, photo_file_name, carrying)
-        self.passenger_seats_count = passenger_seats_count
-
-class Truk(BaseCar):
-    def __init__(self, body_whl=0):
-        self.body_whl = body_whl
-        try:
-            pass  # релизация не валидных значений
-        except:   # ошибка типа, длинны, преобразования, Index?
-            pass
-
-        super(Truk, self).__init__()
-
-
-class SpecMachine(BaseCar):
-    def __init__(self, extra="unknown"):
-        self.extra = extra
-        super(SpecMachine, self).__init__()
-
-car = Car('Bugatti Veyron', 'bugatti.png', '0.312', '2')
-print(car.car_type, car.brand, car.photo_file_name, car.carrying,
-      car.passenger_seats_count, sep='\n')
-print(car.get_photo_file_ext())
-
-
->>>>>>> origin/main
