9ff75a1e0425e07c2f47dc7547bc7f1832c6f3d7
LuckyKAA <kapkaew@yandex.ru>
2021-01-04 13:35:13 +0300

    Just save this files

--- week5/client.py
+++ week5/client.py
@@ -16,35 +16,37 @@ class Client:
         # создаём соединение
         try:
             self.sock = socket.create_connection((host, port), timeout)
-        except socket.error:
-            raise ClientError("соединение не установлено")
+        except socket.error as err:
+            raise ClientError("the connection is not established", err)
 
     def _read_data(self):
         # пытаемся получить ответ от сервера
+        message = b""
         try:
-            message = self.sock.recv(1024).decode("utf-8")
-        except socket.error:
-            raise ClientError("ответ от сервера не получен")
+            while message.endswith(b"\n\n"):
+                message += self.sock.recv(1024).decode("utf-8")
+        except socket.error as err:
+            raise ClientError("no response received from the server", err)
 
         # анализируем результат ответа получаем результат и список с данными
         result, *data = message.split("\n")[:-2]
         if result != "ok":
-            raise ClientError("операция не выполнена")
+            raise ClientError("operation failed")
         return data
 
     def put(self, metric: str, value: float, timestamp=None):
         timestamp = int(time.time()) if timestamp is None else timestamp
         try:
             self.sock.send(f"put {metric} {value} {timestamp}\n".encode("utf-8"))
-        except socket.error:
-            raise ClientError("не удалось отправить данные")
+        except socket.error as err:
+            raise ClientError("failed to send data",  err)
         self._read_data()
 
     def get(self, metric_name):
         try:
             self.sock.send(f"get {metric_name}\n".encode("utf-8"))
-        except socket.error:
-            raise ClientError("не уадлось отправить сообщение")
+        except socket.error as er:
+            raise ClientError("couldn't send a request", er)
 
         data = self._read_data()
         metrics = {}
@@ -60,8 +62,8 @@ class Client:
                 else:
                     metrics[server] = [(int(time_stamp), float(value))]
         # не корректные данные
-        except ValueError:
-            raise ClientError("не корректный формат данных")
+        except ValueError as er:
+            raise ClientError("invalid data format", er)
 
         # сортировка словаря по значению time
         for name in metrics:
@@ -71,5 +73,5 @@ class Client:
     def close(self):
         try:
             self.sock.close()
-        except socket.error:
-            raise ClientError("не удалось закрыть соединение")
+        except socket.error as er:
+            raise ClientError("failed to close connection", er)
